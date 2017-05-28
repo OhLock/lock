e0671bdd20b553e1acf85f95f9d28293221f2257
yiiu <yu.gintoki@gmail.com>
2017-05-28 20:02:17 +0800

    updated config

--- core/middleware/oauth/github.js
+++ core/middleware/oauth/github.js
@@ -9,8 +9,8 @@ export const GithubAuthorize = function (next, error) {
         let cb
         try {
             cb = await axios.post('https://github.com/login/oauth/access_token', {
-                client_id: '6509c38c55d429061594',
-                client_secret: '76f14c9b6c945f197491eb3f7006d1aa89ec22d0',
+                client_id: config.oauth.github.client_id,
+                client_secret: config.oauth.github.client_secret,
                 code: code,
             })
         } catch (err) {
